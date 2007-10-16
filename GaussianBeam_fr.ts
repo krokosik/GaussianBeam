1304c9adb778ee61a2546dec46994fea99f9c78d
lodewyck <lodewyck@37a9f6fc-9735-0410-a863-98db706a8500>
2007-10-16 16:48:18 +0000

    Translation update
    git-svn-id: https://svn.code.sf.net/p/gaussianbeam/code@13 37a9f6fc-9735-0410-a863-98db706a8500

--- GaussianBeam_fr.ts
+++ GaussianBeam_fr.ts
@@ -1,12 +1,15 @@
-<!DOCTYPE TS><TS>
-<defaultcodec></defaultcodec>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="fr">
+<defaultcodec>UTF-8</defaultcodec>
 <context>
     <name>GaussianBeamDelegate</name>
     <message>
+        <location filename="GaussianBeamDelegate.cpp" line="94"/>
         <source>true</source>
         <translation>vrai</translation>
     </message>
     <message>
+        <location filename="GaussianBeamDelegate.cpp" line="95"/>
         <source>false</source>
         <translation>faux</translation>
     </message>
@@ -14,283 +17,388 @@
 <context>
     <name>GaussianBeamForm</name>
     <message>
+        <location filename="GaussianBeamForm.ui" line="13"/>
         <source>Gaussian Beam</source>
         <translation>Gaussian Beam</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="23"/>
         <source>Wavelength</source>
         <translation>Longueur d&apos;onde</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="53"/>
         <source>Add</source>
         <translation>Ajouter</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="60"/>
         <source>Remove</source>
         <translation>Retirer</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="93"/>
         <source>Display</source>
         <translation>Affichage</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="99"/>
         <source>Optics bench</source>
         <translation>Banc optique</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="105"/>
         <source>Horizontal range</source>
         <translation>Échelle horizontale</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="128"/>
         <source>Vertical range</source>
         <translation>Échelle verticale</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="151"/>
         <source>Horizontal offset</source>
         <translation>Décalage horizontal</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="177"/>
         <source>File</source>
         <translation>Fichier</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="183"/>
         <source>Open</source>
         <translation>Ouvrir</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="192"/>
         <source>Save</source>
         <translation>Enregistrer</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="199"/>
         <source>Save as...</source>
         <translation>Enregistrer sous...</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="211"/>
         <source>Print</source>
         <translation>Imprimer</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="241"/>
         <source>Refresh</source>
         <translation>Rafraîchir</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="270"/>
         <source>Magic waist</source>
         <translation>Waist magique</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="276"/>
         <source>Target waist</source>
         <translation>Waist cible</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="339"/>
         <source>Tolerance</source>
         <translation>Tolérance</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="300"/>
         <source> %</source>
         <translation> %</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="319"/>
         <source>Target position</source>
         <translation>Position cible</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="346"/>
         <source> % z0</source>
         <translation> % z0</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="362"/>
         <source>Scramble lenses</source>
         <translation>Mélanger les lentilles</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="389"/>
         <source>Find</source>
         <translation>Trouver</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="438"/>
         <source>Waist fit</source>
         <translation>Ajustement</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="464"/>
         <source>Fit</source>
         <translation>Ajustement</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="481"/>
         <source>Set as input beam</source>
         <translation>Définir comme faisceau incident</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="491"/>
         <source>Set as target beam</source>
         <translation>Définit comme faisceau cible</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="546"/>
         <source>&amp;Lens</source>
         <translation>&amp;Lentille</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="551"/>
         <source>&amp;Flat Interface</source>
         <translation>Interface &amp;plane</translation>
     </message>
     <message>
+        <location filename="GaussianBeamForm.ui" line="556"/>
         <source>&amp;Cuved Interface</source>
         <translation>Interface &amp;courbe</translation>
+    </message>
+    <message>
+        <location filename="GaussianBeamForm.ui" line="221"/>
+        <source>Show Graph</source>
+        <translation>Graphique</translation>
+    </message>
+    <message>
+        <location filename="GaussianBeamForm.ui" line="561"/>
+        <source>Flat &amp;Mirror</source>
+        <translation>&amp;Miroir plan</translation>
+    </message>
+    <message>
+        <location filename="GaussianBeamForm.ui" line="566"/>
+        <source>Cu&amp;rved Mirror</source>
+        <translation>Miroir cou&amp;rbe</translation>
+    </message>
+    <message>
+        <location filename="GaussianBeamForm.ui" line="571"/>
+        <source>Generic &amp;ABCD</source>
+        <translation>&amp;ABCD générique</translation>
     </message>
 </context>
 <context>
     <name>GaussianBeamModel</name>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="126"/>
         <source>Optics</source>
         <translation>Optique</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="130"/>
         <source>Relative
 position</source>
         <translation>Position
 relative</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="142"/>
         <source>Name</source>
         <translation>Nom</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="144"/>
         <source>Lock</source>
         <translation>Vérouillé</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="316"/>
         <source>Input beam</source>
         <translation>Faisceau incident</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="318"/>
         <source>Lens</source>
         <translation>Lentille</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="324"/>
         <source>Flat interface</source>
         <translation>Interface plane</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="326"/>
         <source>Curved interface</source>
         <translation>Interface courbe</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="132"/>
         <source>Properties</source>
         <translation>Propriétés</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="128"/>
         <source>Position</source>
         <translation>Position</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="134"/>
         <source>Waist</source>
         <translation>Waist</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="136"/>
         <source>Waist
 Position</source>
         <translation>Position du
 waist</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="138"/>
         <source>Rayleigh
 length</source>
         <translation>Longueur de
 Rayleigh</translation>
     </message>
     <message>
+        <location filename="GaussianBeamModel.cpp" line="140"/>
         <source>Divergence</source>
         <translation>Divergence</translation>
+    </message>
+    <message>
+        <location filename="GaussianBeamModel.cpp" line="320"/>
+        <source>Flat Mirror</source>
+        <translation>Miroir plan</translation>
+    </message>
+    <message>
+        <location filename="GaussianBeamModel.cpp" line="322"/>
+        <source>Curved Mirror</source>
+        <translation>Miroir courbe</translation>
+    </message>
+    <message>
+        <location filename="GaussianBeamModel.cpp" line="328"/>
+        <source>Generic ABCD</source>
+        <translation>ABCD générique</translation>
     </message>
 </context>
 <context>
     <name>GaussianBeamWidget</name>
     <message>
+        <location filename="GaussianBeamWidget.cpp" line="102"/>
         <source>Radius @ 1/e&#xc2;&#xb2;</source>
         <translation>Rayon @ 1/e²</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidget.cpp" line="103"/>
         <source>Diameter @ 1/e&#xc2;&#xb2;</source>
         <translation>Diamètre @ 1/e²</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidget.cpp" line="281"/>
         <source>Waist</source>
         <translation>Waist</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidget.cpp" line="282"/>
         <source>Position</source>
         <translation>Position</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidget.cpp" line="283"/>
         <source>R&#xc2;&#xb2;</source>
         <translation>R²</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidgetSave.cpp" line="36"/>
         <source>Choose a data file</source>
         <translation>Choisissez un fichier</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidgetSave.cpp" line="43"/>
         <source>Opening file</source>
         <translation>Ouverture du fichier</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidgetSave.cpp" line="43"/>
         <source>Cannot read file %1:
 %2.</source>
         <translation>Ne peut pas lire le fichier %1:
 %2.</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidgetSave.cpp" line="68"/>
         <source>XML error</source>
         <translation>Erreur XML</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidgetSave.cpp" line="55"/>
         <source>Parse error at line %1, column %2:
 %3</source>
         <translation>Erreur de lecture à la ligne %1, colonne %2:
 %3</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidgetSave.cpp" line="63"/>
         <source>The file is not an Gaussian Beam file.</source>
         <translation>Ce fichier n&apos;est pas valide.</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidgetSave.cpp" line="68"/>
         <source>Your version of Gaussian Beam is too old.</source>
         <translation>Votre version de Gaussian Beam est trop ancienne.</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidgetSave.cpp" line="205"/>
         <source>Save File</source>
         <translation>Enregistrer</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidgetSave.cpp" line="215"/>
         <source>Saving file</source>
         <translation>Enregistrement</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidgetSave.cpp" line="215"/>
         <source>Cannot write file %1:
 %2.</source>
         <translation>Ne peut pas écrire dans le fichier %1:
 %2.</translation>
     </message>
     <message>
+        <location filename="GaussianBeamWidget.cpp" line="98"/>
         <source>Value</source>
         <translation>Valeur</translation>
+    </message>
+    <message>
+        <location filename="GaussianBeamWidget.cpp" line="242"/>
+        <source>Desired waist could not be found !</source>
+        <translation>Le waist désiré n&apos;a pas été trouvé !</translation>
     </message>
 </context>
 <context>
     <name>OpticsView</name>
     <message>
+        <location filename="OpticsView.cpp" line="298"/>
         <source>Position: </source>
         <translation>Position : </translation>
     </message>
     <message>
+        <location filename="OpticsView.cpp" line="299"/>
         <source>Beam radius: </source>
         <translation>Rayon : </translation>
     </message>
     <message>
+        <location filename="OpticsView.cpp" line="300"/>
         <source>Beam curvature: </source>
         <translation>Courbure : </translation>
     </message>
     <message>
+        <location filename="OpticsView.cpp" line="300"/>
         <source>mm</source>
         <translation>mm</translation>
     </message>
     <message>
+        <location filename="OpticsView.cpp" line="299"/>
         <source>&#xc2;&#xb5;m</source>
         <translation>µm</translation>
     </message>
