7a60eb5374c77c2c495d1b90a7b22f0b02233781
lodewyck <lodewyck@37a9f6fc-9735-0410-a863-98db706a8500>
2007-11-19 09:39:01 +0000

    Remove release.sh file from release
    git-svn-id: https://svn.code.sf.net/p/gaussianbeam/code@49 37a9f6fc-9735-0410-a863-98db706a8500

--- release.sh
+++ release.sh
@@ -7,5 +7,6 @@ mkdir $RELEASEFOLDER
 svn list | xargs cp -R --target-directory=$RELEASEFOLDER
 find $RELEASEFOLDER | grep "~" | xargs rm -f
 find $RELEASEFOLDER | grep .svn$ | xargs rm -rf
+rm $RELEASEFOLDER/release.sh
 tar -cj $RELEASEFOLDER > $RELEASEFOLDER.tar.bz2
 rm -rf $RELEASEFOLDER
