3d055d759740e1c22f5adf039f2faceacaedc5f5
lodewyck <lodewyck@37a9f6fc-9735-0410-a863-98db706a8500>
2008-08-17 17:28:45 +0000

    Documentation
    git-svn-id: https://svn.code.sf.net/p/gaussianbeam/code@89 37a9f6fc-9735-0410-a863-98db706a8500

--- src/OpticsBench.h
+++ src/OpticsBench.h
@@ -31,13 +31,19 @@
 class OpticsBench;
 
 /**
-* OpticsBenchNotify
+* OpticsBenchNotify. Inherit from this class and register to the bench
+* using OpticsBench::registerNotify(this) if you want to
+* be notified of optics bench change events.
+* Upon registration, (almost) all notifications are called for initilization
 */
 class OpticsBenchNotify
 {
 public:
+	/// Constructor
 	OpticsBenchNotify(OpticsBench& opticsBench);
+	/// Called when the wavevelength changes
 	virtual void OpticsBenchWavelengthChanged() {};
+	/// Called when Optics @p index is added
 	virtual void OpticsBenchOpticsAdded(int /*index*/) {};
 	virtual void OpticsBenchOpticsRemoved(int /*index*/, int /*count*/) {};
 	virtual void OpticsBenchDataChanged(int /*startOptics*/, int /*endOptics*/) {};
@@ -48,6 +54,7 @@ public:
 	virtual void OpticsBenchFitDataChanged(int /*index*/) {};
 
 protected:
+	/// A reference to the OpticsBench to which we register;
 	OpticsBench& m_bench;
 };
 
@@ -61,20 +68,38 @@ public:
 	~OpticsBench();
 
 public:
-	/// Parameters
+
+	// Properties
+
+	/// @return the bench wavelength
 	double wavelength() const { return m_wavelength; }
+	/// Set the bench wavelength to @p wavelength
 	void setWavelength(double wavelength);
+	/// @return the bench left boundary
 	double leftBoundary() { return m_leftBoundary; }
+	/// Set the bench left boundary to @p leftBoundary
 	void setLeftBoundary(double leftBoundary);
+	/// @return the bench right boundary
 	double rightBoundary() { return m_rightBoundary; }
+	/// Set the bench right boundary to @p leftBoundary
 	void setRightBoundary(double rightBoundary);
 
-	/// Handle optics
+	// Optics
+
+	/// @return the number of optics in the bench
 	int nOptics() const { return m_optics.size(); }
+	/**
+	* @return the sorted index of optics @p optics in the optics bench
+	* This index may change when an optics position is changed
+	*/
 	int opticsIndex(const Optics* optics) const;
+	/// @return a pointer to optics situated at index @p index
 	const Optics* optics(int index) const { return m_optics[index]; }
+	/// Add the optics @p optics at index @p index
 	void addOptics(Optics* optics, int index);
+	/// Add an optics of type @p opticsType at index @p index
 	void addOptics(OpticsType opticsType, int index);
+	/// Remove the optics situated at index @p index
 	void removeOptics(int index, int count = 1);
 	/**
 	* Set the optics at @p index to position @p position. Takes care of locks,
@@ -82,16 +107,20 @@ public:
 	* @return the new index of the optics
 	*/
 	int setOpticsPosition(int index, double position, bool respectAbsoluteLock = true);
+	/// set the name of optics at @p index to @p name
 	void setOpticsName(int index, std::string name);
+	/// lock the optics at index @p index to the first optics called @p opticsName
 	void lockTo(int index, std::string opticsName);
+	/// lock the optics at index @p index to the optics which id is @p id
 	void lockTo(int index, int id);
 	/**
-	* Retrieve a non constant pointer to an optics for property change.
-	* When finished, call opticsPropertyChanged()
+	* Retrieve a non constant pointer to the optics @p index for property change.
+	* When finished, call opticsPropertyChanged() to propagate your changes
 	* If opticsBench proposes a direct function for changing a property
 	* (position, name, lockTo), rather use these functions.
 	*/
 	Optics* opticsForPropertyChange(int index) { return m_optics[index]; }
+	/// Call this function after changing the properties of an optics returned by opticsForPropertyChange( @p index )
 	void opticsPropertyChanged(int index);
 
 	/// Beams handling
