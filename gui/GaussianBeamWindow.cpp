83802c16f56f9d6d3b0dac71f4d05fa79b09b302
krokosik <wikkra@gmail.com>
2024-09-10 18:54:51 +0200

    Increase maximum wavelength

--- gui/GaussianBeamWindow.cpp
+++ gui/GaussianBeamWindow.cpp
@@ -83,8 +83,8 @@ GaussianBeamWindow::GaussianBeamWindow(const QString& fileName)
 	m_wavelengthSpinBox = new QDoubleSpinBox(wavelengthWidget);
 	m_wavelengthSpinBox->setDecimals(0);
 	m_wavelengthSpinBox->setSuffix(" nm");
-	m_wavelengthSpinBox->setRange(1., 999999.);
-	m_wavelengthSpinBox->setValue(532.);
+    m_wavelengthSpinBox->setRange(1., 99999999.);
+    m_wavelengthSpinBox->setValue(532.);
 	m_wavelengthSpinBox->setSingleStep(10.);
 	wavelengthLayout->addWidget(m_wavelengthSpinBox);
 	wavelengthLayout->addWidget(wavelengthLabel);
