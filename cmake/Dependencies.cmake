b0065dcc59749636cc884af83e76cff6e9388075
krokosik <wikkra@gmail.com>
2024-09-19 23:40:42 +0200

    Install xml and xslt using fetchcontent

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,5 +1,24 @@
 set(QT_MIN_VERSION "5.15.0")
 find_package(Qt5 COMPONENTS Widgets Test Gui Xml XmlPatterns REQUIRED)
 
-find_package(LibXml2 REQUIRED)
-find_package(LibXslt REQUIRED)
+include(FetchContent)
+
+FetchContent_Declare(
+    LibXml2
+    GIT_REPOSITORY https://gitlab.gnome.org/GNOME/libxml2.git
+    GIT_TAG v2.13.4
+)
+
+FetchContent_Declare(
+    LibXslt
+    GIT_REPOSITORY https://gitlab.gnome.org/GNOME/libxslt.git
+    GIT_TAG v1.1.42
+)
+
+option(LIBXML2_WITH_PYTHON "Build Python bindings" OFF)
+option(LIBXML2_WITH_TESTS "Build tests" OFF)
+option(LIBXSLT_WITH_PYTHON "Build Python bindings" OFF)
+option(LIBXSLT_WITH_TESTS "Build tests" OFF)
+
+FetchContent_MakeAvailable(LibXml2)
+FetchContent_MakeAvailable(LibXslt)
